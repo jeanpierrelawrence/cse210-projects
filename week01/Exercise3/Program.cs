3f3b06f0252a483316bb11e705fc7fc195de0a7e
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-03-01 17:48:43 +0200

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        int guess = 0;
+        int guessCount = 0;
+
+        while (guess != number)
+        {
+            Console.Write("Guess a number between 1 - 10: ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher!");
+            }
+        }
+
+        Console.WriteLine($"You guessed right! It took you {guessCount} guesses.");
     }
 }
