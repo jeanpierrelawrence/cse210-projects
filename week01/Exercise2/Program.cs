b9dc8972e9b80129977623577f2b947012b44f6e
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-03-01 17:22:06 +0200

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        string symbol = "";
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            symbol = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            symbol = "-";
+        }
+
+        if (letter == "A" && symbol == "+")
+        {
+            symbol = "";
+        }
+        if (letter == "F")
+        {
+            symbol = "";
+        }
+
+        Console.WriteLine($"You got a {letter}{symbol}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed! Congradulations!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Retake the course and sign up for free tutoring.");
+        }
     }
 }
