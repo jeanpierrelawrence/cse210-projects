5b0b7465d4b122af54e8c239a5443ff5ee35642e
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-03-02 18:28:04 +0200

    exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        int total = numbers.Sum();
+        double average = numbers.Average();
+        int biggestNumber = numbers.Max();
+
+        Console.WriteLine($"The sum of the numbers in your list is: {total}");
+        Console.WriteLine($"The average of the numbers in your list is: {average}");
+        Console.WriteLine($"The largest number in your list is: {biggestNumber}");
+
+        int minPositive = numbers.Max();
+
+        foreach (int num in numbers)
+        {
+            if (num > 0)
+            {
+                if (num < minPositive)
+                {
+                    minPositive = num;
+                }
+            }
+        }
+
+        Console.WriteLine($"The smallest positive number is: {minPositive}");
+
+        numbers.Sort();
+
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
