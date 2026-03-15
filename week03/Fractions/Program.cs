2cc944f1bb28de774157dc7bb1451bd5d0ed238b
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-03-15 14:24:53 +0200

    fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction defaultFraction = new Fraction();
+        Fraction wholeNumber = new Fraction(5);
+        Fraction custom = new Fraction(6, 7);
+
+        Console.WriteLine(defaultFraction.GetBottom());
+        Console.WriteLine(wholeNumber.GetFractionString());
+
+        custom.SetTop(15);
+        custom.SetBottom(5);
+
+        Console.WriteLine(custom.GetTop());
+        Console.WriteLine(custom.GetDecimalValue());
+
     }
 }
