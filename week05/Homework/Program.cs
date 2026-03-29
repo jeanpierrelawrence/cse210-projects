53cce3b2a6b81d8c1e629e8dcfaa8b7d1bf86098
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-03-29 14:14:16 +0200

    week05 homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("6.1", "4-6", "Roberto Rodriguez", "Fractions");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("The Causes of World War II by Mary Waters", "Mary Waters", "European History");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
