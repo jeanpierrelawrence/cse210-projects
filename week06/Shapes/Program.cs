30650eb118c5a57faded61de130fc86be03884a3
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-04-08 13:10:38 +0200

    week06 shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = [];
+
+        Square square = new Square(5);
+        square.SetColor("Red");
+
+        Rectangle rect = new Rectangle(5, 25);
+        rect.SetColor("Blue");
+
+        Circle circle = new Circle(10.5);
+        circle.SetColor("Orange");
+
+        shapes.Add(square);
+        shapes.Add(rect);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
